7acc9fb5787b001a3077dcec12a1ad1621a08a17
esb179 <esbenn179@gmail.com>
2022-03-10 12:49:14 +0100

    clique now pub

--- src/clique.rs
+++ src/clique.rs
@@ -1,19 +1,19 @@
 pub(crate) struct Clique {
-    preds: Vec<u32>,
-    nodes: Vec<u32>
+    pub preds: Vec<u32>,
+    pub nodes: Vec<u32>
 }
 
 impl Clique {
-    fn new(preds: &Vec<u32>, nodes: &Vec<u32>) -> Self {
+    pub fn new(preds: &Vec<u32>, nodes: &Vec<u32>) -> Self {
         Clique { preds: preds.clone(), nodes:nodes.clone() }
     }
 
-    fn merge(&mut self, c: &Clique) {
+    pub fn merge(&mut self, c: &Clique) {
         self.preds.append(&mut c.preds.clone());
         self.nodes.append(&mut c.nodes.clone());
     }
 
-    fn node_intersection(&self, c: &Clique) -> Vec<u32> {
+    pub fn node_intersection(&self, c: &Clique) -> Vec<u32> {
         let mut intersection: Vec<u32> = Vec::new();
 
         for node in &self.nodes {
