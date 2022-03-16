8a464f0fa72cf96a4f1bed705c3309840ee4bb4b
esb179 <esbenn179@gmail.com>
2022-03-16 10:48:54 +0100

    index map + clique tests

---
+++ src/index_map.rs
@@ -0,0 +1,25 @@
+use std::collections::HashMap;
+
+use crate::clique::Clique;
+
+fn get_index_map(source_clique: &Vec<Clique>, target_clique: &Vec<Clique>) -> HashMap<u32, [u32; 2]> {
+    let mut index_map: HashMap<u32, [u32; 2]> = HashMap::new();
+
+    for (i, c) in source_clique.iter().enumerate() {
+        for p in c.preds.clone() {
+            index_map.insert(p, [i as u32, 0]);
+        }
+        for n in c.nodes.clone() {
+            index_map.insert(n, [i as u32, 0]);
+        }
+    }
+
+    for (i, c) in target_clique.iter().enumerate() {
+        for p in c.preds.clone() {
+            let x = index_map.get_mut(&p).unwrap();
+            x[1] = i as u32;
+        }
+    }
+
+    return index_map;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod triple_parser;
 mod meta_parser;
 mod clique;
+mod index_map;
+mod clique_tests;
 
 fn main() {
     println!("Hello, world!");
