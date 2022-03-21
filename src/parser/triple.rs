df56a30f36bf9b0e397429fc4244152cb338bfa0
esb179 <esbenn179@gmail.com>
2022-03-21 12:39:15 +0100

    Triple::new

--- src/parser/triple.rs
+++ src/parser/triple.rs
@@ -13,31 +13,38 @@ pub struct Triple {
     pub is_type: bool,
 }
 
+impl Triple {
+    pub fn new(line: &String, dict: &HashMap<&str, u32>) -> Self {
+        let line_splits: Vec<&str> = line.split(" ").collect();
+        let mut is_type_pred = false;
+
+        if line_splits[1] == TYPE_STRING {
+            is_type_pred = true;
+        }
+
+        Triple {
+            sub: *dict.get(line_splits[0]).unwrap(),
+            pred: *dict.get(line_splits[1]).unwrap(),
+            obj: *dict.get(line_splits[2]).unwrap(),
+            is_type: is_type_pred,
+        }
+    }
+    
+    const TYPE_STRING: &'static str = "<http://www.w3.org/1999/02/22-rdf-syntax-ns#type>";
+}
+
 pub fn push_triples_into_vector(
     triple_path: &str,
-    dict: &HashMap<String, u32>,
+    dict: &HashMap<&str, u32>,
 ) -> Result<Vec<Triple>, io::Error> {
     let file = File::open(triple_path)?;
     let reader = BufReader::new(file);
 
     let mut vector_of_triples: Vec<Triple> = Vec::new();
-    let mut is_type_pred = false;
 
     for line in reader.lines() {
         let line = line?;
-        let line_splits: Vec<&str> = line.split(" ").collect();
-
-        if line_splits[1] == TYPE_STRING {
-            is_type_pred = true;
-        }
-        //GetID does not exist. Its pseudo
-        vector_of_triples.push(Triple {
-            sub: *dict.get(line_splits[0]).unwrap(),
-            pred: *dict.get(line_splits[1]).unwrap(),
-            obj: *dict.get(line_splits[2]).unwrap(),
-            is_type: is_type_pred,
-        });
-        //println!("{}", line_splits[i])
+        vector_of_triples.push(Triple::new(&line, dict));
     }
     Ok(vector_of_triples)
 }
