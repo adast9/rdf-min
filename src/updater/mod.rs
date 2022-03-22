259b9d977faa821f205cd826cdb692f0d5ccfdc1
adamstuck <adam.moloney.stuck@gmail.com>
2022-03-22 11:38:57 +0100

    merge + started triple updater

--- src/updater/mod.rs
+++ src/updater/mod.rs
@@ -37,5 +37,38 @@ fn handle_insersertions(
 ) {
     for ins in insertions {
         let changes = clique_updater::get_changes(index_map, &ins, source_clique, target_clique);
+        let snodes = get_super_nodes(changes, source_clique, target_clique, index_map);
     }
 }
+
+fn get_super_nodes(
+    changes: Vec<Clique_Change>,
+    source_clique: &mut Vec<Clique>,
+    target_clique: &mut Vec<Clique>,
+    index_map: &mut HashMap<u32, [usize; 2]>, // triples: &mut Vec<Triple>,
+                                              // supernodes: &mut HashMap<u32, Vec<u32>>,
+                                              // nodes: &mut HashMap<u32, NodeInfo>
+) {
+    // for each change
+    for cc in changes {
+        if cc.is_source {
+            let sc = source_clique[cc.clique_index].clone();
+
+            let mut super_nodes: Vec<Vec<u32>> = Vec::new();
+
+            for nn in cc.new_nodes {
+                let index = index_map.get(&nn).unwrap()[1];
+                let tc = target_clique[index].clone();
+
+                let intersect = sc.node_intersection(&tc);
+                if intersect.len() > 0 {
+                    super_nodes.push(intersect);
+                }
+            }
+        }
+    }
+}
+
+// fn handle_clique_change() {
+//     unimplemented!();
+// }
