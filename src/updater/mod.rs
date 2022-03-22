68a48ae56fd5a317e1cf12abe4e3c64087402408
adamstuck <adam.moloney.stuck@gmail.com>
2022-03-22 13:42:50 +0100

    get supernodes for a clique change

--- src/updater/mod.rs
+++ src/updater/mod.rs
@@ -1,9 +1,6 @@
-use std::collections::HashMap;
-
-use crate::parser::{clique::Clique, index_map, meta_parser::NodeInfo, triple::Triple};
-
 use self::clique_updater::Clique_Change;
-
+use crate::parser::{clique::Clique, index_map, meta_parser::NodeInfo, triple::Triple};
+use std::collections::HashMap;
 mod clique_updater;
 
 pub fn run(
@@ -47,30 +44,41 @@ fn get_super_nodes(
     changes: Vec<Clique_Change>,
     source_clique: &mut Vec<Clique>,
     target_clique: &mut Vec<Clique>,
-    index_map: &mut HashMap<u32, [usize; 2]>, // triples: &mut Vec<Triple>,
-                                              // supernodes: &mut HashMap<u32, Vec<u32>>,
-                                              // nodes: &mut HashMap<u32, NodeInfo>
+    index_map: &mut HashMap<u32, [usize; 2]>,
 ) {
     // for each change
     for cc in changes {
-        if cc.is_source {
-            let sc = source_clique[cc.clique_index].clone();
-
-            let mut super_nodes: Vec<Vec<u32>> = Vec::new();
-
-            for nn in cc.new_nodes {
-                let index = index_map.get(&nn).unwrap()[1];
-                let tc = target_clique[index].clone();
-
-                let intersect = sc.node_intersection(&tc);
-                if intersect.len() > 0 {
-                    super_nodes.push(intersect);
-                }
-            }
-        }
+        handle_clique_change(cc, source_clique, target_clique, index_map);
     }
 }
 
-// fn handle_clique_change() {
-//     unimplemented!();
-// }
+fn handle_clique_change(
+    change: Clique_Change,
+    source_clique: &mut Vec<Clique>,
+    target_clique: &mut Vec<Clique>,
+    index_map: &mut HashMap<u32, [usize; 2]>,
+) -> Vec<Vec<u32>> {
+    let mut super_nodes: Vec<Vec<u32>> = Vec::new();
+
+    let c1 = if change.is_source {
+        source_clique[change.clique_index].clone()
+    } else {
+        target_clique[change.clique_index].clone()
+    };
+
+    for node in change.new_nodes {
+        let c2 = if change.is_source {
+            let index = index_map.get(&node).unwrap()[1];
+            target_clique[index].clone()
+        } else {
+            let index = index_map.get(&node).unwrap()[0];
+            source_clique[index].clone()
+        };
+
+        let intersect = c1.node_intersection(&c2);
+        if intersect.len() > 0 {
+            super_nodes.push(intersect);
+        }
+    }
+    return super_nodes;
+}
