bd0da5f8e9d1707b2c2e55204c3bfd9b0764bfd3
Alan Khorsid <alan18@hotmail.dk>
2022-03-29 12:47:35 +0200

    update index map ding ding dong

--- src/updater/triple_updater.rs
+++ src/updater/triple_updater.rs
@@ -1,18 +1,41 @@
 use std::collections::HashMap;
 
-use crate::parser::{triple::Triple, Stuff};
+use crate::{parser::{triple::Triple, Stuff}, util::generate_new_id};
 
-/*
-pub fn update_triples(stuff: Stuff, dict: &mut HashMap<String, u32>) {
-    for triple in stuff.triples {
-        let mut new_key = generate_new_id(dict);
-        for (sn1, sn11) in stuff.supernodes.into_iter() {
+pub fn update_triples(stuff: &mut Stuff, snodes: &Vec<Vec<u32>>) {
+    for snode in snodes {
+        let new_key = generate_new_id(&stuff.dict);
+        for triple in &mut stuff.triples {
+            update_id(triple, snode, &mut stuff.index_map, new_key);
+        }
+    }
+}
+
+fn update_id(triple: &mut Triple, snode: &Vec<u32>, index_map: &mut HashMap<u32, [usize; 2]>, new_key: u32, stuff: &mut Stuff) {
+    let mut value: [usize; 2] = [0, 0];
+    let mut is_inserted = false;
+    for node in snode {
+        if &triple.sub == node { 
+            is_inserted = true;
+
+            triple.sub = new_key; 
+
+            value = index_map.get(node).unwrap().clone();
+            index_map.remove(node);
+        } 
+        if &triple.obj == node { 
+            is_inserted = true;
+
+            triple.obj = new_key;
+
+            value = index_map.get(node).unwrap().clone();
+            index_map.remove(node);
+        } 
+        if is_inserted == true { 
+            index_map.insert(new_key, value); 
+            let m = stuff.dict.get(node).unwrap();
 
         }
     }
-}*/
-
-fn write_to_file() {
-
 }
 
