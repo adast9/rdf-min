b4a3d5d7bd7c5f64669d60c962932bbe170000b7
Adam <adam.moloney.stuck@gmail.com>
2022-04-15 15:38:11 +0200

    Better writer structure

--- src/util/io.rs
+++ src/util/io.rs
@@ -1,6 +1,4 @@
-use crate::classes::dataset::Dataset;
 use io::{BufReader, Error};
-use std::fs::remove_file;
 use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
@@ -8,11 +6,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::path::PathBuf;
 
-use crate::classes::dict::Dict;
-use crate::classes::meta::Meta;
-use crate::classes::triple::Triple;
-
-fn write_lines(path: &PathBuf, vec: &Vec<String>) -> Result<(), Error> {
+pub fn write_lines(path: &PathBuf, vec: &Vec<String>) -> Result<(), Error> {
     let mut file = OpenOptions::new()
         .create(true)
         .write(true)
@@ -23,36 +17,6 @@ fn write_lines(path: &PathBuf, vec: &Vec<String>) -> Result<(), Error> {
         writeln!(file, "{}", s)?
     }
     Ok(())
-}
-
-pub fn write_triples(path: &PathBuf, dataset: &Dataset) -> Result<(), Error> {
-    let mut triple_strings: Vec<String> = Vec::new();
-
-    for triple in &dataset.triples.data_triples {
-        let sub_string = dataset.key_by_value(&triple.sub).unwrap();
-        let pred_string = dataset.key_by_value(&triple.pred).unwrap();
-        let obj_string = dataset.key_by_value(&triple.obj).unwrap();
-
-        let triple_string = format!("{} {} {} .", sub_string, pred_string, obj_string);
-
-        if !triple_strings.contains(&triple_string) {
-            triple_strings.push(triple_string);
-        }
-    }
-    Ok(write_lines(path, &triple_strings)?)
-}
-
-pub fn write_dict(path: &PathBuf, dataset: &Dataset) -> Result<(), Error> {
-    if path.exists() {
-        remove_file(path)?;
-    }
-    Ok(write_lines(path, &dataset.dict_strings())?)
-}
-
-pub fn write_meta(path: &PathBuf, meta: &Meta) -> Result<(), Error> {
-    let data = meta.serialize();
-    let file_str = serde_json::to_string(&data)?;
-    Ok(write_lines(path, &vec![file_str])?)
 }
 
 pub fn read_lines<P>(path: &P) -> io::Result<Vec<String>>
--- src/writer/mod.rs
+++ src/writer/mod.rs
@@ -1,21 +1,54 @@
 use crate::classes::dataset::Dataset;
 use crate::classes::meta::Meta;
-use crate::util;
+use crate::util::io;
 use crate::Config;
-use std::fs;
+use std::fs::create_dir;
+use std::fs::remove_file;
+use std::io::Error;
+use std::path::PathBuf;
 
 pub fn run(config: &Config, dataset: &Dataset, meta: &Meta) {
-    util::io::write_triples(
+    write_triples(
         &config.dataset_path.parent().unwrap().join("summary.nt"),
         &dataset,
     )
     .unwrap();
 
     if config.use_fast {
-        fs::create_dir(&config.meta_folder_path).unwrap();
+        create_dir(&config.meta_folder_path).unwrap();
     }
 
-    util::io::write_dict(&config.meta_folder_path.join("dict"), &dataset).unwrap();
+    write_dict(&config.meta_folder_path.join("dict"), &dataset).unwrap();
 
-    util::io::write_meta(&config.meta_folder_path.join("meta.json"), &meta).unwrap();
+    write_meta(&config.meta_folder_path.join("meta.json"), &meta).unwrap();
+}
+
+fn write_triples(path: &PathBuf, dataset: &Dataset) -> Result<(), Error> {
+    let mut triple_strings: Vec<String> = Vec::new();
+
+    for triple in &dataset.triples.data_triples {
+        let sub_string = dataset.key_by_value(&triple.sub).unwrap();
+        let pred_string = dataset.key_by_value(&triple.pred).unwrap();
+        let obj_string = dataset.key_by_value(&triple.obj).unwrap();
+
+        let triple_string = format!("{} {} {} .", sub_string, pred_string, obj_string);
+
+        if !triple_strings.contains(&triple_string) {
+            triple_strings.push(triple_string);
+        }
+    }
+    Ok(io::write_lines(path, &triple_strings)?)
+}
+
+fn write_dict(path: &PathBuf, dataset: &Dataset) -> Result<(), Error> {
+    if path.exists() {
+        remove_file(path)?;
+    }
+    Ok(io::write_lines(path, &dataset.dict_strings())?)
+}
+
+fn write_meta(path: &PathBuf, meta: &Meta) -> Result<(), Error> {
+    let data = meta.serialize();
+    let file_str = serde_json::to_string(&data)?;
+    Ok(io::write_lines(path, &vec![file_str])?)
 }
